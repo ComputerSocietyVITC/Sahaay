8cee9a5a8aa9c5c19e2f979378e00b7326a48ee3
cool-pants <muban.sg@gmail.com>
2021-10-02 12:55:43 +0530

    [testing]: added new tests for patch

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
+from fastapi.testclient import TestClient
+from app import app
+
 
 from tests.fixtures.userFix import (user, no_pass_user,
                                     unmatching_pass_user,
                                     no_fname_user, invalid_fname_user,
-                                    unreg_role_user, empty_role_user) 
+                                    unreg_role_user, empty_role_user, update_def_user) 
+
+@pytest.fixture
+def test_client():
+    client = TestClient(app=app)
+    yield client
--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,10 +1,4 @@
-from fastapi.testclient import TestClient
-
-from app import app
-
 import pytest
-
-client = TestClient(app)
 
 @pytest.mark.usefixtures("user", "no_pass_user",
                         "unmatching_pass_user", "no_fname_user",
@@ -13,25 +7,19 @@ client = TestClient(app)
 def test_create_user(user,no_pass_user,
                     unmatching_pass_user,
                     no_fname_user, invalid_fname_user,
-                    unreg_role_user, empty_role_user):
+                    unreg_role_user, empty_role_user, test_client):
     
-    response = client.post("/user",json = user)
-    assert response.status_code == 201
+    url = "/user"
     
-    response = client.post("/user",json = no_pass_user)
-    assert response.status_code == 422
+    _data = [user, no_pass_user, unmatching_pass_user, no_fname_user, invalid_fname_user, unreg_role_user, empty_role_user]
+    _response = [201, 422, 422, 422, 422, 422, 422]
     
-    response = client.post("/user",json = unmatching_pass_user)
-    assert response.status_code == 422
+    for i in range(len(_data)):
+        response = test_client.post(url,json = _data[i])
+        assert response.status_code == _response[i]
     
-    response = client.post("/user",json = no_fname_user)
-    assert response.status_code == 422
-    
-    response = client.post("/user",json = invalid_fname_user)
-    assert response.status_code == 422
-    
-    response = client.post("/user",json = unreg_role_user)
-    assert response.status_code == 422
-    
-    response = client.post("/user",json = empty_role_user)
-    assert response.status_code == 422
+@pytest.mark.usefixtures("update_def_user","user")
+def test_update_user(update_def_user, user, test_client):
+    url = "/user/update"
+    response = test_client.patch(url, params = {"email":user["email"]}, json=update_def_user)
+    assert  response.status_code == 200
