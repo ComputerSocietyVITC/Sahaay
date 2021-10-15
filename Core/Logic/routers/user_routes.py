bcdaba094f45ae77f4e079b27b84b15f779b58a0
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-15 10:26:34 +0530

    Experimental changes to the router

--- Core/Logic/routers/user_routes.py
+++ Core/Logic/routers/user_routes.py
@@ -1,13 +1,36 @@
-from fastapi import (
-    APIRouter,
-    requests,
-    Request,    
-)
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 userRouter = APIRouter()
 
-
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+    
 @userRouter.get("/hello-world")
 def Hello_world(request: Request):
+    print(list(request.user))
     return HTMLResponse("Hello World")
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+@userRouter.get("/items/")
+async def read_items(current_user: User = Depends(get_current_user)):
+    return current_user
