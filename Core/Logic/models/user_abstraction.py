15f70dd1226dcb64160aa1e75e1170ef74f38b1e
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-17 22:35:13 +0530

    PEP8 Formatting changes

--- Core/Logic/models/user_abstraction.py
+++ Core/Logic/models/user_abstraction.py
@@ -3,7 +3,20 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 
 
-class UserModel(AbstractUser): 
-    Reg_no = models.CharField(max_length = 9, unique = True, editable = True, null = False, blank = False, default = '00VIT----')
-    UiD = models.CharField(max_length = 255, unique = True, editable = False, null = False, blank = False, default = uuid.uuid4)
-    
+class UserModel(AbstractUser):
+    Reg_no = models.CharField(
+        max_length=9,
+        unique=True,
+        editable=True,
+        null=False,
+        blank=False,
+        default="00VIT----",
+    )
+    UiD = models.CharField(
+        max_length=255,
+        unique=True,
+        editable=False,
+        null=False,
+        blank=False,
+        default=uuid.uuid4,
+    )
