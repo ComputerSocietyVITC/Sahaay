e11499e1b791ae722b8bbc261ecf512dbed6e997
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2021-10-17 22:28:17 +0530

    Update user_abstraction.py

--- Core/Logic/models/user_abstraction.py
+++ Core/Logic/models/user_abstraction.py
@@ -5,17 +5,5 @@ import uuid
 
 class UserModel(AbstractUser): 
     Reg_no = models.CharField(max_length = 9, unique = True, editable = True, null = False, blank = False, default = '00VIT----')
-    UiD = models.CharField(max_length = 9, unique = True, editable = False, null = False, blank = False, default ='defo12345')
+    UiD = models.CharField(max_length = 255, unique = True, editable = False, null = False, blank = False, default = uuid.uuid4)
     
-    def save(self, reg_edited=False, *args, **kwargs) -> None:
-        # Code to ensure a new UiD is generated when first user is created
-        # Only the reg_no prefix gets updated if only Reg_no is updated
-        # To be changed if better logic can be found
-        
-        uid = self.UiD
-        if uid == 'defo12345' or reg_edited == True:
-            reg_no = self.Reg_no
-            self.UiD = reg_no[:4]+ ( str(uuid.uuid4().hex[:5]) if reg_edited == False else  self.UiD[-5:])
-        
-        super().save(*args, **kwargs)
-        
