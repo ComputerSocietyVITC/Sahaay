9b5d93e0f0ff513eb3f21ebcf74c21afd5fbb0b2
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-19 01:05:35 +0530

    Addition of user

--- Core/Logic/routers/user_routes.py
+++ Core/Logic/routers/user_routes.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
 from django.contrib.auth import authenticate
-from fastapi import APIRouter, Depends, Form, HTTPException, requests
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, Form, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from starlette import requests
 
 user_router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="./token")
@@ -16,12 +16,12 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user_data = UserModel.objects.filter(username=form_data.username)
     if not user_data:
         raise HTTPException(
-            status_code=417, detail="Incorrect User name, the query was not found"
+            status_code = 417, detail="Incorrect User name, the query was not found"
         )
 
-    user = authenticate(username=form_data.username, password=form_data.password)
+    user = authenticate(username = form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(status_code = 400, detail="Incorrect username or password")
 
     return {"access_token": user.username, "token_type": "bearer"}
 
@@ -31,6 +31,17 @@ async def forms(username: str = Form(...), password: str = Form(...)):
     return {"username": username}
 
 
-@user_router.get("/current-user")
-def return_current_user(request):
-    return request.user
+async def get_current_user(request):
+    from Logic.models import UserModel
+    user = UserModel.objects.filter(username = request.user)
+    if not user:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+@user_router.get("/users/me")
+async def read_users_me(request: Request):
+    return (request.user)
