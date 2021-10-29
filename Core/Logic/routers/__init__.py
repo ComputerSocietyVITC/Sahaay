0d573edf1226d3bc350da07fb3b10bbbee74c8dd
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-29 17:08:26 +0530

    Addition of administrator router which deals with GET, PUT and DELETE method

--- Core/Logic/routers/__init__.py
+++ Core/Logic/routers/__init__.py
@@ -1,2 +1,3 @@
-__all__ = "user_router"
+__all__ = ["user_router", "admin_router"]
 from .user_routes import user_router as user_router
+from .admin_routes import admin as admin_router
--- Core/Logic/routers/user_routes.py
+++ Core/Logic/routers/user_routes.py
@@ -1,29 +1,7 @@
-from django.contrib.auth import authenticate
-from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.security import HTTPBasicCredentials
-from fastapi.security.http import HTTPBasic
+from fastapi import APIRouter,Request
 
 user_router = APIRouter()
-security = HTTPBasic()
 
-
-@user_router.post("/login")
-def login(
-    request: Request, credentials: HTTPBasicCredentials = Depends(security)
-):
-    from Logic.models import UserModel
-
-    user_data = UserModel.objects.filter(username=credentials.username)
-    if not user_data:
-        raise HTTPException(
-            status_code=417, detail="Incorrect User name, the query was not found"
-        )
-
-    user = authenticate(username=credentials.username, password=credentials.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    if user and request.method == "POST":
-        return request.headers
 
 
 @user_router.get("/users/me")
