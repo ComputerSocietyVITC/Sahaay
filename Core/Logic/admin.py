33ee71e45eb24b878063756e888adb73c4137205
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-30 23:47:48 +0530

    empty logo

--- Core/Logic/admin.py
+++ Core/Logic/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import UserModel, CommentsTable
 
-admin.site.register(UserModel)
+admin.site.register([UserModel, CommentsTable])
