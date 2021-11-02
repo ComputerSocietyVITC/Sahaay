62e773f8b55f257ef1443a2e80d313880ac09e4f
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-11-02 12:11:58 +0530

    PEP8 changes

--- Core/Logic/routers/user_routes.py
+++ Core/Logic/routers/user_routes.py
@@ -4,6 +4,7 @@ from pydantic.types import UUID4
 import starlette
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 from pydantic import EmailStr
+
 user_router = APIRouter()
 
 
@@ -15,8 +16,10 @@ class PydanticUserModel(BaseModel):
     email: EmailStr
     Reg_no: str
 
+
 class DeleteUserModel(BaseModel):
-    Reg_no:str
+    Reg_no: str
+
 
 @user_router.get("users/current-user")
 async def current_user(request: Request):
@@ -26,31 +29,44 @@ async def current_user(request: Request):
 @user_router.get("/all-users")
 def show_all_users():
     from Logic.models import UserModel
-    return list(UserModel.objects.values('Reg_no'))
+
+    return list(UserModel.objects.values("Reg_no"))
+
 
 @user_router.post("/user")
 def create_user(user: PydanticUserModel):
     from Logic.models import UserModel
-    pseudouser = UserModel(username = user.username, first_name = user.first_name, last_name = user.last_name, email = user.email, Reg_no=user.Reg_no)
+
+    pseudouser = UserModel(
+        username=user.username,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        Reg_no=user.Reg_no,
+    )
     pseudouser.set_password(user.password)
     pseudouser.save()
     return f"User got created! {HTTP_201_CREATED}"
 
+
 @user_router.delete("/user")
 def delete_user(user: DeleteUserModel):
     from Logic.models import UserModel
-    pseudouser = UserModel.objects.filter(Reg_no = user.Reg_no)
+
+    pseudouser = UserModel.objects.filter(Reg_no=user.Reg_no)
     if pseudouser:
         pseudouser.delete()
-        return  f"The given user was deleted: {user.Reg_no}"
-    else: 
+        return f"The given user was deleted: {user.Reg_no}"
+    else:
         return f"User not found! HTTP {HTTP_404_NOT_FOUND}"
+
 
 @user_router.put("/user")
 def update_user(user: PydanticUserModel):
     from Logic.models import UserModel
+
     try:
-        pseudouser = UserModel.objects.get(Reg_no = user.Reg_no)
+        pseudouser = UserModel.objects.get(Reg_no=user.Reg_no)
         if pseudouser:
             pseudouser.first_name = user.first_name
             pseudouser.last_name = user.last_name
@@ -59,15 +75,12 @@ def update_user(user: PydanticUserModel):
             pseudouser.save()
             return f"{pseudouser} was saved"
     except Exception:
-            val = UserModel(username = user.username, first_name = user.first_name, last_name = user.last_name, email = user.email, Reg_no=user.Reg_no)
-            val.save()
-            return f"The user {val} was saved! HTTP {HTTP_200_OK}"
-
-@user_router.get("/users/{user_id}")
-def get_user(user_id: str):
-    from Logic.models import UserModel
-    user = UserModel.objects.filter(Reg_no=user_id)
-    if user:
-        return {"user":list(user)}
-    else:
-        return {"user": "empty"}
+        val = UserModel(
+            username=user.username,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+            Reg_no=user.Reg_no,
+        )
+        val.save()
+        return f"The user {val} was saved! HTTP {HTTP_200_OK}"
